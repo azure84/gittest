fea23646f4e1a76110e8773c22f0f2e1dbe8e477
Kangsoo Jung <jung@Kangsooui-MacBook-Air.local>
2014-12-29 19:26:12 +0900

    Initial Commit

---
+++ MusicVisualizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicVisualizer
+//
+//  Created by Kangsoo Jung on 2014. 12. 29..
+//  Copyright (c) 2014년 Jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
